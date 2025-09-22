d3a6a84019ffb55726d6ec41789387cd0b7c999c
mauroperez055 <mauroperez055@gmail.com>
2025-09-22 19:55:55 -0300

    Backend con base de datos final

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 require('dotenv').config(); // Importamos las variables de entorno
 const express = require('express'); // Importamos el módulo express
-const cors = require('cors'); // Importamos el módulo cors
 const Note = require('./models/note'); 
 
-const app = express(); // Creamos una aplicación de express
+// Creamos una aplicación de express
+const app = express(); 
 
-app.use(cors()); // Usamos el middleware cors para permitir solicitudes desde cualquier origen
-
-// json-parser middleware
-app.use(express.json()); // Middleware para parsear el cuerpo de las peticiones como JSON, toma los datos json de una solicitud, los transforma en objetos de JavaScript y los agrega a la propiedad body del objeto request
-
-app.use(express.static('dist')); // Middleware para servir archivos estáticos desde la carpeta 'dist'
+// Creamos un array de notas 
+let notes = [] 
 
 // Middleware que imprime informacion sobre cada peticion
 const requestLogger = (request, response, next) => {
@@ -21,42 +17,24 @@ const requestLogger = (request, response, next) => {
   next(); // Pasamos al siguiente middleware
 }
 
-app.use(requestLogger); // Usamos el middleware requestLogger
+// Usamos el middleware requestLogger
+app.use(requestLogger); 
 
-//const http = require('http'); // Importamos el módulo http
+// Middleware para servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static('dist')); 
 
-let notes = [ // Creamos un array de notas 
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true   
-  }
-]
+// Middleware para parsear el cuerpo de las peticiones como JSON, toma los datos json de una solicitud, los transforma en objetos de JavaScript y los agrega a la propiedad body del objeto request
+app.use(express.json()); 
 
-/* const app = http.createServer((request, response) => { // Creamos el servidor 
-  response.writeHead(200, { 'Content-Type': 'application/json' }); // Configuramos la cabecera de la respuesta 
-  response.end(JSON.stringify(notes)); // Enviamos la respuesta al cliente 
-}) */
-
-
-app.get('/', (request, response) => { // Definimos una ruta para la raíz del servidor
-  response.send('<h1>Backend de app de notas funcionando</h1>'); // Enviamos una respuesta al cliente
+// Definimos una ruta para la raíz del servidor y enviamos una respuesta al cliente
+app.get('/', (request, response) => { 
+  response.send('<h1>Backend de app de notas funcionando</h1>'); 
 })
 
 // Endpoint para obtener todas las notas
-app.get('/api/notes', (request, response) => { // Definimos una ruta para /api/notes
+app.get('/api/notes', (request, response) => { 
   Note.find({}).then(notes => {
-    response.json(notes); // Enviamos la respuesta al cliente en formato JSON 
+    response.json(notes); 
   })
 })
 
@@ -66,34 +44,6 @@ app.get('/api/notes/:id', (request, response) => {
     response.json(note);
   })
 })
-
-/* // Endpoint para obtener una nota por su id
-app.get('/api/notes/:id', (request, response) => { // Definimos una ruta para /api/notes/:id
-  const id = Number(request.params.id); // Obtenemos el id de la nota de los parámetros de la ruta
-  const note = notes.find(note => note.id === id); // Buscamos la nota con el id correspondiente
-
-  if (note) { // Si encontramos la nota 
-    response.json(note); // Enviamos la respuesta al cliente en formato JSON
-  } else {
-    response.status(404).end(); // Si no encontramos la nota, enviamos un error 404
-  }
-}) */
-
-// Endpoint para eliminar una nota por su id
-app.delete('/api/notes/:id', (request, response) => { 
-  const id = Number(request.params.id); // Obtenemos el id de la nota de los parámetros de la ruta
-  notes = notes.filter(note => note.id !== id); // Filtramos las notas para eliminar la nota con el id correspondiente
-
-  response.status(204).end(); // Enviamos una respuesta al cliente con el código 204 (No Content)
-})
-
-// Función para generar un id único para una nueva nota
-/* const generateId = () => {
-  const maxId = notes.length > 0 // notes.length es mayor que 0? 
-    ? Math.max(...notes.map(n => n.id)) // notes.map(n => n.id) es un array, con ... lo convertimos en una lista de argumentos para Math.max, que devuelve el id máximo
-    : 0; // Si no, el id máximo es 0
-  return maxId + 1; // Devolvemos el id máximo + 1
-} */
 
 // Endpoint para crear una nueva nota
 app.post('/api/notes', (request, response) => { 
@@ -114,19 +64,28 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote);
   })
-
-  /* notes = notes.concat(note); // Agregamos la nueva nota al array de notas
-  response.json(note); // Enviamos la respuesta al cliente en formato JSON */
 })
+
+// Endpoint para eliminar una nota por su id
+app.delete('/api/notes/:id', (request, response) => { 
+  const id = Number(request.params.id); // Obtenemos el id de la nota de los parámetros de la ruta
+  notes = notes.filter(note => note.id !== id); // Filtramos las notas para eliminar la nota con el id correspondiente
+
+  response.status(204).end(); // 204: No Content
+})
+
 
 // Middleware para capturar solicitudes a endpoints o rutas desconocidas
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' }); // Enviamos una respuesta al cliente con el código 404 (Not Found)
+  response.status(404).send({ error: 'unknown endpoint' });
 }
 
-app.use(unknownEndpoint); // Usamos el middleware unknownEndpoint
+// Usamos el middleware unknownEndpoint
+app.use(unknownEndpoint); 
 
 const PORT = process.env.PORT;
+
+// Ponemos el servidor a escuchar en el puerto definido por la variable de entorno
 app.listen(PORT, () => { 
   console.log(`Server running on port ${PORT}`); 
-}); // Ponemos el servidor a escuchar en el puerto definido
+}); 
