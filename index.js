742a431b7662aa33e4b793cd691fb3fcb27caf5a
mauroperez055 <mauroperez055@gmail.com>
2025-09-25 23:24:08 -0300

    Actualizo el backend

--- index.js
+++ index.js
@@ -1,91 +1,150 @@
-require('dotenv').config(); // Importamos las variables de entorno
-const express = require('express'); // Importamos el módulo express
-const Note = require('./models/note'); 
+require("dotenv").config(); // Importamos las variables de entorno
+const express = require("express"); // Importamos el módulo express
+const Note = require("./models/note");
 
 // Creamos una aplicación de express
-const app = express(); 
+const app = express();
 
-// Creamos un array de notas 
-let notes = [] 
+
+// ---------------------
+// 1. MIDDLEWARES GENERALES
+// ---------------------
+
+//1- Middleware para servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static("dist"));
+
+//2- Middleware para parsear el cuerpo de las peticiones como JSON, toma los datos json de una solicitud, los transforma en objetos de JavaScript y los agrega a la propiedad body del objeto request
+app.use(express.json());
 
 // Middleware que imprime informacion sobre cada peticion
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method); // Mostramos el método de la petición
-  console.log('Path:  ', request.path); // Mostramos la ruta de la petición
-  console.log('Body:  ', request.body); // Mostramos el cuerpo de la petición
-  console.log('---'); // Mostramos una línea para separar las peticiones
+  console.log("Method:", request.method); // Mostramos el método de la petición
+  console.log("Path:  ", request.path); // Mostramos la ruta de la petición
+  console.log("Body:  ", request.body); // Mostramos el cuerpo de la petición
+  console.log("---"); // Mostramos una línea para separar las peticiones
   next(); // Pasamos al siguiente middleware
-}
+};
 
-// Usamos el middleware requestLogger
-app.use(requestLogger); 
+//3- Usamos el middleware requestLogger
+app.use(requestLogger);
 
-// Middleware para servir archivos estáticos desde la carpeta 'dist'
-app.use(express.static('dist')); 
 
-// Middleware para parsear el cuerpo de las peticiones como JSON, toma los datos json de una solicitud, los transforma en objetos de JavaScript y los agrega a la propiedad body del objeto request
-app.use(express.json()); 
+// ---------------------
+// 2. RUTAS
+// ---------------------
 
 // Definimos una ruta para la raíz del servidor y enviamos una respuesta al cliente
-app.get('/', (request, response) => { 
-  response.send('<h1>Backend de app de notas funcionando</h1>'); 
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Backend de app de notas funcionando</h1>");
+});
 
 // Endpoint para obtener todas las notas
-app.get('/api/notes', (request, response) => { 
-  Note.find({}).then(notes => {
-    response.json(notes); 
-  })
-})
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
 // Obtener una nota individual con el metodo findById de mongoose
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note);
-  })
-})
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {  
+      if (note) { 
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error)); // Pasamos el error al siguiente middleware de manejo de errores
+});
 
 // Endpoint para crear una nueva nota
-app.post('/api/notes', (request, response) => { 
+app.post("/api/notes", (request, response) => {
   const body = request.body; // Obtenemos el cuerpo de la petición
 
-  if (body.content === undefined) { // Si el cuerpo no tiene contenido
-    return response.status(400).json({ // Enviamos una respuesta al cliente con el código 400 (Bad Request)
-      error: 'content missing' // Mensaje de error
+  if (body.content === undefined) {
+    // Si el cuerpo no tiene contenido
+    return response.status(400).json({
+      // Enviamos una respuesta al cliente con el código 400 (Bad Request)
+      error: "content missing", // Mensaje de error
     });
   }
 
-  const note = new Note ({ // Creamos un objeto nota
+  const note = new Note({
+    // Creamos un objeto nota
     content: body.content, // Asignamos el contenido de la nota
     important: body.important || false, // Asignamos la importancia de la nota, si no se especifica, por defecto es false
     /* id: generateId(), // Generamos un id único para la nota */
-  })
+  });
 
-  note.save().then(savedNote => {
+  note.save().then((savedNote) => {
     response.json(savedNote);
-  })
-})
+  });
+});
 
 // Endpoint para eliminar una nota por su id
-app.delete('/api/notes/:id', (request, response) => { 
-  const id = Number(request.params.id); // Obtenemos el id de la nota de los parámetros de la ruta
-  notes = notes.filter(note => note.id !== id); // Filtramos las notas para eliminar la nota con el id correspondiente
+app.delete("/api/notes/:id", (request, response) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end(); // Enviamos una respuesta al cliente con el código 204 (No Content)
+    })
+    .catch(error => next(error)); // Pasamos el error al siguiente middleware de manejo de errores
+});
 
-  response.status(204).end(); // 204: No Content
+// Endpoint para actualizar una nota por su id
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  // Creamos un objeto nota con el contenido y la importancia actualizados
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  //Actualizamos la nota y con new:true hacemos que la promesa devuelva la nota actualizada y no la nota antigua
+  Note.findByIdAndUpdate(request.params.id, note, {new: true}) 
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch(error => next(error));
 })
 
+
+// ---------------------
+// 3. MIDDLEWARES DE CONTROL DE ERRORES
+// ---------------------
 
 // Middleware para capturar solicitudes a endpoints o rutas desconocidas
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 // Usamos el middleware unknownEndpoint
-app.use(unknownEndpoint); 
+app.use(unknownEndpoint);
+
+// Middleware para manejar errores
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message); // Mostramos el mensaje de error en la consola
+
+  if (error.name === 'CastError') { // Si el error es un CastError (error de conversión de tipo)
+    // Enviamos una respuesta al cliente con el código 400 (Bad Request) y un mensaje de error
+    return response.statuts(400).send({ error: 'malformatted id' }); 
+  }
+
+  next(error); // Pasamos el error al siguiente middleware
+}
+
+// Usamos el middleware errorHandler
+app.use(errorHandler);
+
+
+// ---------------------
+// 4. INICIAR SERVIDOR
+// ---------------------
 
 const PORT = process.env.PORT;
 
 // Ponemos el servidor a escuchar en el puerto definido por la variable de entorno
-app.listen(PORT, () => { 
-  console.log(`Server running on port ${PORT}`); 
-}); 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
