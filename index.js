1dc9763bfa87d4c2d21f38ed2370bbd2c7515d8f
mauroperez055 <mauroperez055@gmail.com>
2025-10-09 21:34:07 -0300

    prueba de n8n

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('./app'); // Importamos la aplicación de express
 const config = require("./utils/config"); 
 const logger = require("./utils/logger"); 
 
+//cambio para ver si funciona n8n
+
 // Ponemos el servidor a escuchar en el puerto definido por la variable de entorno
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`); // Usamos el logger para mostrar el mensaje en la consola
