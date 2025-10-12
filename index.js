e51b29d6f08c41acc7fcced94b6a220d3c84eb8c
mauroperez055 <mauroperez055@gmail.com>
2025-10-12 20:48:54 -0300

    Actualizo backend: testing

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const app = require('./app'); // Importamos la aplicación de express
 const config = require("./utils/config"); 
 const logger = require("./utils/logger"); 
 
-//sdasdsadasdsadsadasdasdsa
-
 // Ponemos el servidor a escuchar en el puerto definido por la variable de entorno
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`); // Usamos el logger para mostrar el mensaje en la consola
